0883463d1104ccd10125907a22f29438579b6e58
Muhammad-Al-Kindy <al.kindy0000@gmail.com>
2023-08-04 10:04:20 +0700

    first commit

---
+++ database/migrations/2023_08_03_024152_create_mahasiswas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nim', 20)->unique();
+            $table->string('Nama', 100);
+            $table->string('Kelas',10);
+            $table->string('Jurusan',30);
+            $table->string('No_Handphone',15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
